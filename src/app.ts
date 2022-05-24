936d810cdc1c2027f9b68abc5897efed30241f52
jesusglez96 <jezu96@gmail.com>
2022-05-24 18:49:35 +0200

    Update

--- src/app.ts
+++ src/app.ts
@@ -2,41 +2,52 @@
 /* eslint-disable no-void */
 /* eslint-disable import/no-unresolved */
 import { join } from 'path';
-import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
-// eslint-disable-next-line import/no-unresolved
-// eslint-disable-next-line import/extensions
+import AutoLoad from '@fastify/autoload';
+import { fastify, FastifyInstance } from 'fastify';
 import MongoDB from './utils/mongodb';
 import options from './utils/swagger-config';
+// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/extensions
+const chalk = require('chalk');
+const log = require('fancy-log');
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
+const serverFastify: FastifyInstance = fastify();
+// export type AppOptions = {
+//   // Place your custom options for app below here.
+// } & Partial<AutoloadPluginOptions>;
 
-const app: FastifyPluginAsync<AppOptions> = async (
-  fastify,
-  opts,
-): Promise<void> => {
-  // Place here your custom code!
-  await MongoDB.connectDB();
-  void fastify.register(require('@fastify/swagger'), options);
-  // Do not touch the following lines
+// const app: FastifyPluginAsync<AppOptions> = async (
+//   fastify,
+//   opts,
+// ): Promise<void> => {
+//   // Place here your custom code!
+MongoDB.connectDB();
+void serverFastify.register(require('@fastify/swagger'), options);
+// Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts,
-  });
+//   // This loads all plugins defined in plugins
+//   // those should be support plugins that are reused
+//   // through your application
+void serverFastify.register(AutoLoad, {
+  dir: join(__dirname, 'plugins'),
+  // options: opts,
+});
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts,
-  });
-};
+//   // This loads all plugins defined in routes
+//   // define your routes in one of these
+void serverFastify.register(AutoLoad, {
+  dir: join(__dirname, 'routes'),
+  // options: opts,
+});
+// };
 
-export default app;
-export { app };
+// export default app;
+// export { app };
+
+serverFastify.listen(String(process.env.PORT), (err: any, address: any) => {
+  if (err) {
+    log.error(chalk.red(err));
+    process.exit(1);
+  }
+  log.info(chalk.blue(`Server listening at ${address}`));
+});
